ca366e5c0876d642f932bc9f1ac204e48169c1b1
Bernardo Amorim <contato@bamorim.com>
2016-12-15 04:12:07 -0200

    BFGS base, not working

--- main.c
+++ main.c
@@ -15,10 +15,10 @@
 #define debug(a) (0)
 #endif
 
-inline void printm(double * mat, int m, int n) {
+inline void printm(double * mat, int n, int m) {
   #ifdef DEBUG
-  for (int i = 0; i < m; i++){
-    for (int j = 0; j < n; j++){
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
       printf("%g ", mat[i*m+j]);
     }
     printf("\n");
@@ -41,10 +41,47 @@ void ggf(double * x, double * g){
 }
 
 // Helpers
-void mul(double * m, double * v, double * o){
-  // Only 2x2 * 2x1, don't bother implementing algorithm for this hahaha
-  o[0] = m[0]*v[0] + m[1] * v[1];
-  o[1] = m[2]*v[0] + m[3] * v[1];
+void scalar_mul(double * a, double c, int n, double * o){
+  for(int i = 0; i < n; i++){
+    o[i] *= c;
+  }
+}
+void add(double * a, double * b, int n, double * o){
+  for(int i = 0; i < n; i++){
+    o[i] = a[i] + b[i];
+  }
+}
+void id(double * o, int n, int m){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(i==j){
+        o[i*m + j] = 1;
+      } else {
+        o[i*m + j] = 0;
+      }
+    }
+  }
+}
+void clone(double * a, int n, double * o){
+  for(int i = 0; i < n; i++){
+    o[i] = a[i];
+  }
+}
+void mul(double * a, double * b, int n, int m, int p, double * o){
+  // a: n x m
+  // aik = a[i*m + k]
+  // b: m x p
+  // bkj = a[k*p + j]
+  // o: n x p
+  // oij = [i*p + j]
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < p; j++){
+      o[i*p + j] = 0;
+      for(int k = 0; k < m; k++){
+        o[i*p + j] += a[m*i + k] * b[p*k + j];
+      }
+    }
+  }
 }
 
 void invert(double * im, double * om){
@@ -160,7 +197,7 @@ long newt_method(double * x){
 
     ggf(x,gg);
     invert(gg,gg);
-    mul(gg,g,d);
+    mul(gg,g,2,2,1,d);
 
     d[0] = -d[0];
     d[1] = -d[1];
@@ -171,22 +208,115 @@ long newt_method(double * x){
   }
 }
 
+long bfgs(double * x){
+  long iter = 0;
+  double g[2];
+  // H0 = I
+  double h[4];
+
+  double q[2];
+  double p[2];
+  double w1[4];
+  double w2[4];
+  double r;
+
+  double d[2] = {0,0};
+  gf(x,g);
+  ggf(x,h);
+  invert(h,h);
+  double t;
+  while(1){
+    iter++;
+    if(fabs(g[0]) < EPSILON && fabs(g[1]) < EPSILON || iter > ITER_MAX)
+      return iter;
+
+    mul(h, g, 2, 2, 1, d);
+    scalar_mul(d, -1, 2, d);
+    t = aurea(x, d);
+
+    // d = t*d
+    scalar_mul(d, t, 2, d);
+
+    // p = -xk
+    scalar_mul(x,-1, 2, p);
+    // q = -gk
+    scalar_mul(g,-1, 2, q);
+
+    // xk = xk + tk*dk
+    add(x, d, 2, x);
+    // gk = \/f
+    gf(x,g);
+
+    // pk+1 = pk+1 - pk
+    add(x,p,2,p);
+    // qk+1 = gk+1 - gk
+    add(g,q,2,q);
+    // r = 1/qTp
+    mul(q,p,1,2,1,w1);
+    r = w1[0];
+    // w1 = I - r*p*qT
+    mul(p,q,2,1,2,w1);
+    scalar_mul(w1,-1 * r,4,w1);
+    id(w2,2,2);
+    add(w2,w1,4,w1);
+
+    // h = (I - r*p*qT)*H
+    mul(w1,h,2,2,2,w2);
+    clone(w2,4,h);
+
+    // w1 = I - r*q*pT
+    mul(q,p,2,1,2,w1);
+    scalar_mul(w1,-1 * r,4,w1);
+    id(w2,2,2);
+    add(w2,w1,4,w1);
+
+    // h = (I - r*p*qT)*H*(I - r*q*pT)
+    mul(h,w1,2,2,2,w2);
+    clone(w2,4,h);
+
+    // w1 = r*p*pT
+    mul(p,p,2,1,2,w1);
+    scalar_mul(w1,r,4,w1);
+
+    // h = (I - r*p*qT)*H*(I - r*q*pT) + r*p*pT
+    add(h,w1,4,h);
+  }
+}
+
 void run(long (*runner)(double * x)){
-  double x[2] = {0.01,0.01};
-  printf("x0 = (%.10e,%.10e)\n", x[0], x[1]);
-  printf("f(x0) = (%.10e)\n", f(x[0], x[1]));
-  long iter = runner(x);
-  printf("iter = %d\n", iter);
-  printf("x* = (%.10e,%.10e)\n", x[0], x[1]);
-  printf("f(x*) = (%.10e)\n", f(x[0], x[1]));
+  double xs[10][2] = {
+    {-0.6,0.7},
+    {1,0},
+    {0.001,0.001},
+    {0.5, 0.5},
+    {0.4, 0.6},
+    {0.6, 0.4},
+    {0.2, -0.2},
+    {0.1, 0.1},
+    {-0.1, -0.5},
+    {0.3, 0.01}
+  };
+
+  for(int i = 0; i < 10; i ++){
+    printf("x0 = (%.10e,%.10e)\n", xs[i][0], xs[i][1]);
+    printf("f(x0) = (%.10e)\n", f(xs[i][0], xs[i][1]));
+    long iter = runner(xs[i]);
+    printf("iter = %d\n", iter);
+    printf("x* = (%.10e,%.10e)\n", xs[i][0], xs[i][1]);
+    printf("f(x*) = (%.10e)\n", f(xs[i][0], xs[i][1]));
+    printf("---------\n");
+  }
 }
 
 int main(){
-
   printf("Gradiente\n");
   run(grad_method);
-  printf("\n\nNewton\n");
+  printf("\n\n\nNewton\n");
   run(newt_method);
+  printf("\n\n\nBFGS\n");
+  run(bfgs);
+
+  printf("\n=============END=============\n\n");
 
   return 0;
 }
